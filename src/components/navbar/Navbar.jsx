3ec08ae473d4364b73566f67f7726af66a2d2124
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-27 16:45:05 +0200

    Update responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,26 +7,26 @@ import logo from "./Goog.png";
 export default function Navbar() {
   return (
     <Container fluid>
-      <div class="row align-items-center margin justify-content-end">
+      <div class="row align-items-center margin justify-content-between">
         <div class="col-md-2">
           <img src={logo} alt="" />
         </div>
-        <div class="col-md-1 offset-md-5">
+        <div class="col-lg-1 col-md-2 offset-lg-5">
           <Nav.Item>
             <Nav.Link eventKey="link-1">Home</Nav.Link>
           </Nav.Item>
         </div>
-        <div class="col-md-1">
+        <div class="col-md-2 col-lg-1">
           <Nav.Item>
             <Nav.Link eventKey="link-1">About</Nav.Link>
           </Nav.Item>
         </div>
-        <div class="col-md-1">
+        <div class="col-md-2 col-lg-1">
           <Nav.Item>
             <Nav.Link eventKey="link-1">Work</Nav.Link>
           </Nav.Item>
         </div>
-        <div class="col-md-1">
+        <div class="col-md-2 col-lg-1">
           <Nav.Item>
             <Nav.Link eventKey="link-1">Contact</Nav.Link>
           </Nav.Item>
