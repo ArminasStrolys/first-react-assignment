a54d0fd8ec4e01696c296030021f3744bfd866a2
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-27 16:29:58 +0200

    Mini updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from "./Goog.png";
 export default function Navbar() {
   return (
     <Container fluid>
-      <div class="row align-items-end margin justify-content-end">
+      <div class="row align-items-center margin justify-content-end">
         <div class="col-md-2">
           <img src={logo} alt="" />
         </div>
